274df58f2d1e4ac53f6738bac2ce928262513564
Basti Destruction <basti@dest.cc>
2018-07-02 01:13:26 +0200

    add autoconfiguration as client

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -3,7 +3,17 @@
 if [ ! "$(ls -A /usr/local/etc/icinga2)" ]; then
 	echo "=> Copying fresh config-files for /usr/local/etc/icinga2"
 	cp -r /usr/local/etc/icinga2.dist/* /usr/local/etc/icinga2/
-    #(cd /usr/local/etc/icinga2/conf.d && rm services.conf downtimes.conf notifications.conf groups.conf templates.conf commands.conf hosts.conf users.conf)
+
+    echo "=> Setting up connection to master node"
+    icinga2 node setup \
+        --zone $ICINGA2_CLIENT_HOSTNAME \
+        --endpoint $ICINGA2_MASTER_FQDN \
+        --parent_host $ICINGA2_MASTER_FQDN \
+        --ticket $ICINGA2_CLIENT_TICKET \
+        --trustedcert /opt/master.crt \
+        --accept-config \
+        --accept-commands \
+        --disable-confd
 fi
 
 mkdir -p /usr/local/var/cache/icinga2
@@ -18,6 +28,21 @@ chown -R icinga:icinga /usr/local/var/run/icinga2
 mkdir -p /usr/local/var/lib/icinga2
 chown -R icinga:icinga /usr/local/var/lib/icinga2
 
-echo "Sleeping now..."
+echo "=> Starting Icinga"
 
-sleep 10000000000000000000000
+icinga2 daemon &
+
+# Allow any signal which would kill a process to stop server
+trap "killall icinga2" HUP INT QUIT ABRT ALRM TERM TSTP
+
+i=0
+while [ $i -lt 5 ] ; do
+    sleep 5
+    if pgrep -x "icinga2" > /dev/null
+    then
+        i=0
+    else
+        i=$[$i+1]
+        echo "=> No icinga process found ($i)"
+    fi
+done
